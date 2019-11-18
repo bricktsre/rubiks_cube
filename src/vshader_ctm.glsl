1e33dc5b82b336c02f9f2d3f11b4a803a91cb489
greg-laptop <gwoodward21@gmail.com>
2019-11-18 14:16:18 -0500

    Fixing lighting stuff

--- src/vshader_ctm.glsl
+++ src/vshader_ctm.glsl
@@ -14,22 +14,27 @@ uniform mat4 model_view;
 uniform mat4 projection;
 uniform vec4 light_position;
 uniform int isShadow;
-uniform vec4 sPosition;
 
 void main()
 {
 	if(isShadow == 1){
-		float x = (light_position[0]-light_position[1])*((light_position[0]-vPosition[0])/(light_position[1]-vPosition[1]));
+		vec4 tempV = ctm * vPosition;
+		float x = (light_position[0]-light_position[1])*((light_position[0]-tempV[0])/(light_position[1]-tempV[1]));
 		float y = 0.01;
-		float z = (light_position[2]-light_position[1])*((light_position[2]-vPosition[2])/(light_position[1]-vPosition[1]));
-		gl_Position = projection * model_view * ctm * vec4(x,y,z,1.0);
+		float z = (light_position[2]-light_position[1])*((light_position[2]-tempV[2])/(light_position[1]-tempV[1]));
+		gl_Position = projection * model_view * vec4(x,y,z,1.0);
 		N = projection * model_view * ctm * vNormal;
-		L = projection * model_view * ctm * (light_position - vec4(x,y,z,1.0));
+		L = projection * model_view * (light_position - vec4(x,y,z,1.0));
 		vec4 eye_point = vec4(0.0, 0.0, 0.0, 1.0);
-		V = eye_point - (projection * model_view * ctm * vec4(x,y,z,1.0));
-		distance = length(L);
-	}
-	else{	gl_Position = projection * model_view * ctm * vPosition;
+		V = eye_point - (projection * model_view * vec4(x,y,z,1.0));
+		/*gl_Position = model_view * vec4(x,y,z,1.0);
+		N = projection * model_view * ctm * vNormal;
+		L = model_view * (light_position - vec4(x,y,z,1.0));
+		vec4 eye_point = vec4(0.0, 0.0, 0.0, 1.0);
+		V = eye_point - (model_view * vec4(x,y,z,1.0));
+		*/distance = length(L);
+	}else{
+		gl_Position = projection * model_view * ctm * vPosition;
 		N = projection * model_view * ctm * vNormal;
 		L = projection * model_view * ctm * (light_position - vPosition);
 		vec4 eye_point = vec4(0.0, 0.0, 0.0, 1.0);
